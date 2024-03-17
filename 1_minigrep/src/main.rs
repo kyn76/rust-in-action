bf795967deec1704b0df12501df1a81e18ee37ad
kyn76 <simon.dcmbl@gmail.com>
2024-03-17 15:45:22 +0100

    add clap for argument parsing

--- 1_minigrep/src/main.rs
+++ 1_minigrep/src/main.rs
@@ -1,17 +1,12 @@
-use std::env;
 use std::process;
 use minigrep::Config;
+use clap::Parser;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let config = Config::parse();
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
